67ee21658db90c6bd909efb25399f3acf2b548da
Alguien <mpaulinodurand2015@gmail.com>
2025-10-27 16:49:34 -0500

    archivos

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,4 +46,9 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("com.google.android.material:material:1.12.0") // O una versión reciente
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.viewpager2:viewpager2:1.0.0")
 }
